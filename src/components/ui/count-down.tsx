d999696e42bfe86f76ce0517bdb1b93a09c2ce2e
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-03-10 01:40:13 +0300

    feat(countdown): added count down in the underconstruction page

---
+++ src/components/ui/count-down.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+const Countdown = ({ targetDate }: { targetDate: string }) => {
+  // Calculate the time difference between now and the target date
+  const calculateTimeLeft = () => {
+    const difference = +new Date(targetDate) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    // Clear the interval on component unmount
+    return () => clearTimeout(timer);
+  });
+
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-2 justify-center">
+      {Object.keys(timeLeft).map((interval: string) => (
+        <div key={interval} className="flex flex-col items-center gap-1">
+          <span className="text-sm font-medium tracking-wide text-gray-500 md:text-base dark:text-gray-400">
+            {interval.charAt(0).toUpperCase() + interval.slice(1)}
+          </span>
+          <span className="text-xl font-semibold md:text-2xl">
+            {timeLeft[interval as keyof typeof timeLeft]}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Countdown;
