3661a8b5de60a16ff74647342740aea5203874f9
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-03-12 23:59:11 +0300

    fix: project cards finished with out errors

--- src/layout/card-stacks.tsx
+++ src/layout/card-stacks.tsx
@@ -1,15 +1,17 @@
 "use client";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { experience, sections } from "../../utils/objects/setions";
+import { experience, projects, sections } from "../../utils/objects/setions";
 import { ExternalLink, MoveUpRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { BorderButton } from "@/components/ui/moving-border";
+import { Card } from "@/components/ui/card";
+import Image from "next/image";
 
 export const StickyScroll = () => {
   return (
-    <div className="w-full">
-      <ScrollArea className="h-screen md:pr-10 md:pt-20 w-full  md:flex md:flex-col">
-        <div className="lg:flex  flex-col gap-4  w-full">
+    <ScrollArea className="w-full h-screen ">
+      <div className="h-screen md:pr-10 md:pt-20 w-full   md:flex md:flex-col">
+        <div className="lg:flex  flex-col gap-10 py-10    w-full">
           <div className="flex flex-col gap-4  w-full z-50 ">
             {sections.map((section, index) => (
               <div className="w-full" key={index}>
@@ -23,11 +25,11 @@ export const StickyScroll = () => {
               EXPERIENCE
             </div>
 
-            <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-4">
               {experience.map((data, index) => {
                 return (
                   <div
-                    className="cursor-pointer group  flex flex-row gap-4 items-start w-full hover hover:bg-white/10 dark:hover:bg-primary/10 transition-all hover:border hover:backdrop-blur-2xl hover:rounded-2xl py-6 px-4 md:px-10 "
+                    className="cursor-pointer group  flex flex-row gap-4 items-start w-full hover hover:bg-gray-300/20  transition-all hover:border backdrop-blur-4xl hover:rounded-2xl py-6 px-4 md:px-10 "
                     key={index}
                   >
                     <div className="w-1/3 text-primary/60 group-hover:text-primary">
@@ -52,7 +54,7 @@ export const StickyScroll = () => {
                             <BorderButton
                               borderRadius="1rem"
                               key={index}
-                              className="bg-white dark:bg-black text-black dark:text-white  border-neutral-200 dark:border-slate-800"
+                              className="bg-white dark:bg-black text-black dark:text-white  border-neutral-200 dark:border-slate-800 group-hover:font-semibold"
                             >
                               <div className="m-1 text-xs">{value}</div>
                             </BorderButton>
@@ -65,14 +67,63 @@ export const StickyScroll = () => {
               })}
             </div>
           </div>
-          <Button
-            variant={"link"}
-            className="w-fit flex gap-2 flex-row text-xl"
-          >
+          <Button className="w-fit flex gap-2 flex-row text-xl mx-auto my-10">
             View Full Resume <MoveUpRight />
           </Button>
+          <div className="sticky top-0  px-6 py-4 backdrop-blur-3xl z-40 font-bold leading-snug tracking-wider">
+            PROJECTS
+          </div>
+          <div className="flex flex-col gap-4">
+            {projects.map((data, index) => {
+              return (
+                <div
+                  className="cursor-pointer group  flex flex-col gap-4 items-start w-full hover hover:bg-gray-300/20  transition-all hover:border backdrop-blur-4xl hover:rounded-2xl py-6 px-4 md:px-10 "
+                  key={index}
+                >
+                  <div
+                    className={`gap-6 items-start w-full ${
+                      index % 2 === 1
+                        ? "flex md:flex-row flex-col"
+                        : "flex md:flex-row-reverse flex-col"
+                    }`}
+                  >
+                    <Image
+                      src={data.image}
+                      alt={data.title}
+                      width="500"
+                      height="500"
+                      className="border border-green-500 w-full h-60"
+                    />
+                    <div className="flex flex-col gap-2 w-full">
+                      <p className="text-xl font-bold">{data.title}</p>
+                      <p className="line-clamp-3 text-sm">{data.content}</p>
+                      <div className="flex flex-row gap-4 items-center">
+                        link
+                        <ExternalLink className="h-5 w-5" />
+                      </div>
+                      <div className="flex flex-row flex-wrap gap-2">
+                        {data.techStacks.map((value, index) => (
+                          <BorderButton
+                            borderRadius="1rem"
+                            key={index}
+                            className="bg-white dark:bg-black text-black dark:text-white  border-neutral-200 dark:border-slate-800 group-hover:font-semibold"
+                          >
+                            <div className="m-1 text-xs">{value}</div>
+                          </BorderButton>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <Button className="w-fit flex gap-2 flex-row text-xl mx-auto my-10">
+            Project Achieves
+            <MoveUpRight />
+          </Button>
         </div>
-      </ScrollArea>
-    </div>
+      </div>
+    </ScrollArea>
   );
 };
--- utils/objects/setions.tsx
+++ utils/objects/setions.tsx
@@ -7,7 +7,7 @@ export const experience = [
     timeline: "Aug 30 - Present",
     project: "E-commerce Web App: Digital Mekato",
     content: `The individual significantly accelerated the development of the online store by 45% by implementing smart ...`,
-    techstack:['React','Nextjs','Typescript','Tailwindcss','Laravel','Mysql']
+    techstack: ["Front End", "Back End"],
   },
   {
     title: "Software Engineer ",
@@ -15,7 +15,7 @@ export const experience = [
     timeline: "Jan 30 - Present",
     project: "E-commerce Web Dashboard for Ethiochef",
     content: `  The individual played a key role in establishing secure and efficient client-server communication for an...`,
-    techstack:[,'Nextjs','Typescript','Tailwindcss','Express','Postgres']
+    techstack: [, "Back End", "Front End", "Full stack"],
   },
   {
     title: " Student Parliament President",
@@ -23,7 +23,7 @@ export const experience = [
     timeline: "Oct 21 - 2023",
     project: "Vice President for three semesters | President for one Semester",
     content: `The individual effectively led the student parliament, improving student life and communication with the university...`,
-    techstack:['Leadership','President','Vice President']
+    techstack: ["Leadership", "President", "Vice President"],
   },
 ];
 export const sections = [
@@ -50,4 +50,81 @@ export const sections = [
   },
 ];
 
-
+export const projects = [
+  {
+    image: "",
+    title: "Ethiochef Client Side & Admin Dashboard",
+    content:
+      "The food recipe web app integrated diverse tech and multiple payment methods for 200k+ users. Back-end optimization with Redis achieved a 60% performance boost, and CI/CD pipelines accelerated releases by 40%. The architecture supported customizable subscriptions, and React with Shadcn UI improved user engagement by 30%.The food recipe web app integrated diverse tech and multiple payment methods for 200k+ users. Back-end optimization with Redis achieved a 60% performance boost, and CI/CD pipelines accelerated releases by 40%. The architecture supported customizable subscriptions, and React with Shadcn UI improved user engagement by 30%.",
+    link: "https://ethiochef.com",
+    techStacks: [
+      "Front End",
+      "Back end",
+      "Nextjs",
+      "Tailwindcss",
+      "Shadcn UI",
+      "Express",
+      "Prisma",
+      "Mysql",
+    ],
+  },
+  {
+    image: "",
+    title: "Digital Merkato Client Side & Admin Dashboard",
+    content:
+      "Digital Merkato, an E-commerce, streamlined development with CI/CD pipelines, boosting speed by 45%, and integrated payment methods like Chapa for enhanced financial capabilities. Prioritizing user experience, the platform utilized React and Zustand for reusable components and advanced state management. Architectural improvements led to a 75% increase in system efficiency, and a versatile company dashboard reduced project development time by 50%.",
+    link: "https://digitalmerkato.co",
+    techStacks: [
+      "Front End",
+      "Reactjs",
+      "Tailwindcss",
+      "Shadcn UI",
+      "Zustand",
+      "React Query",
+    ],
+  },
+  {
+    image: "",
+    title: "Shewa-Mart Client Side & Admin Dashboard",
+    content:
+      "Shewa-Mart is an e-commerce platform that used CI/CD pipelines to optimize its development process, resulting in a 45% speed increase and the integration of Chapa for enhanced financial functions. The platform used Zustand and React for advanced state management and reusable components, with an emphasis on the user experience. Improvements to the architecture led to a significant 75% gain in system efficiency, and a flexible business dashboard helped to cut project development time by 50%.",
+    link: "https://shewa-mart.com",
+    techStacks: [
+      "Front End",
+      "Reactjs",
+      "Tailwindcss",
+      "Shadcn UI",
+      "Zustand",
+      "React Query",
+    ],
+  },
+  {
+    image: "",
+    title: "Edu Bus Admin Dashboard",
+    content:
+      "Edu Bus ensures safe and efficient school transportation, providing real-time tracking and financial tools for schools, parents, and car providers. Parents receive immediate feedback and secure payments, car providers benefit from streamlined navigation, and schools gain control through a dedicated dashboard. Overall, Edu Bus enhances the school transportation experience for all stakeholders.",
+    link: "https://education-bus-system-admin.vercel.app/",
+    techStacks: [
+      "Front End",
+      "Reactjs",
+      "Tailwindcss",
+      "Shadcn UI",
+      "Zustand",
+      "React Query",
+    ],
+  },
+  {
+    image: "",
+    title: "Edu Bus Mobile App",
+    content:
+      "Edu Bus ensures safe and efficient school transportation, providing real-time tracking and financial tools for schools, parents, and car providers. Parents receive immediate feedback and secure payments, car providers benefit from streamlined navigation, and schools gain control through a dedicated dashboard. Overall, Edu Bus enhances the school transportation experience for all stakeholders.",
+    techStacks: [
+      "Front End",
+      "Reactjs",
+      "Tailwindcss",
+      "Shadcn UI",
+      "Zustand",
+      "React Query",
+    ],
+  },
+];
