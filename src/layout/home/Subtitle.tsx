f9a7ab3997e28213af135491e2c0104d9bf6e81f
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-03-16 01:14:28 +0300

    fix: used entity elements for build errors

--- src/layout/home/Subtitle.tsx
+++ src/layout/home/Subtitle.tsx
@@ -4,11 +4,12 @@ export function SubTitle() {
   return (
     <div className="flex items-center justify-center md:items-start  w-full overflow-hidden px-4">
       <div className="tracking-wider leading-loose text-[#6d7075] text-center md:text-left text-sm">
-        <b className="text-primary">Abel Shibabaw</b>, a software engineer from{" "}
-        <b className="text-primary">Addis Ababa</b>, with{" "}
+        <b className="text-primary">Abel Shibabaw</b>&comma; a software engineer
+        from <b className="text-primary">Addis Ababa</b>&comma; with{" "}
         <b className="text-primary">3+ years</b> experience. Specializing in{" "}
-        <b className="text-primary">full-stack</b> development, passionate about
-        innovative solutions. Let's create impactful technology together.
+        <b className="text-primary">full-stack</b> development&comma; passionate
+        about innovative solutions. Let&lsquo;s create impactful technology
+        together.
       </div>
     </div>
   );
