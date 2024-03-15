5fd5d3899a7c7010c2d3bde7f094320e6b4037a5
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-03-16 01:17:17 +0300

    fix: used entity elements for build errors

--- src/layout/home/floating-nav-bar.tsx
+++ src/layout/home/floating-nav-bar.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useEffect, useState } from "react";
 import { Button } from "../../components/ui/button";
 import { CardContainer, CardItem } from "@/components/ui/acertinity-card";
 import {
@@ -19,22 +18,10 @@ const FloatingNavBar = ({
     link: string;
   }[];
 }) => {
-  const [mobile, setMobile] = useState<boolean>(false);
-  useEffect(() => {
-    const handleResize = () => {
-      setMobile(window.innerWidth <= 768);
-    };
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
   return (
     <div className="hover:scale-105 fixed   z-50 bottom-10 left-4 transition-all  lg:left-[38%] md:left-1/3 ease-linear">
       <CardContainer className="max-md:w-full ">
         <CardItem
-          translateX={mobile ? "" : "-20"}
           className={`glass-container bg-white dark:bg-black/30 bg-opacity-25 backdrop-blur-2xl border-2 max-md:border-b-0 max-md:pt-4   rounded-full  max-md:w-full max-md:items-center max-md:justify-center max-md:scale-0 shadow-xl flex flex-row gap-4 py-2 px-3`}
         >
           {menus.map((menu) => (
@@ -64,4 +51,4 @@ const FloatingNavBar = ({
   );
 };
 
-export default dynamic(() => Promise.resolve(FloatingNavBar), { ssr: false })
+export default dynamic(() => Promise.resolve(FloatingNavBar), { ssr: false });
