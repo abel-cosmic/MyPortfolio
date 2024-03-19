ed8261a2662af804aae324445b3712ab52e1a618
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-03-20 01:14:24 +0300

    fix:added links

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Resend } from "resend";
-export default async function POST(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
