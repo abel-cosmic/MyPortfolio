da2d32edcdbd19551c40b2a0a7dc40ed2fcfd24c
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-04-25 20:07:47 +0300

    added backend

--- src/components/projects/back-end.tsx
+++ src/components/projects/back-end.tsx
@@ -1,92 +1,57 @@
 "use client";
+
+import ProjectCard from "../card/project-card";
+
 const BackEnd = () => {
+  const BackProjects = [
+    {
+      image: "",
+      title: "Ethiochef Backend",
+      description:
+        "Designed and built both frontend and backend for a recipie app with 200,000+ users. This app has more 170+ endpoints and major payment getway integrations like Chapa, ET-Switch, Abysinnia Bank, Telebirr, and more.",
+      link: "https://ethiochef.com",
+      techStacks: ["Front End", "Back end", "Express", "Prisma", "Mysql"],
+    },
+    {
+      image: "",
+      title: "PrepMaster (Backend In Development)",
+      description:
+        "An E-learning SAAS aiming to help students are universtiees get prepared for thier upcoming exams. This app has more than 100+ endpoints and also contains an admin dashboard for admins to manage their items.",
+      link: "https://prep-master-vite.vercel.app/",
+      techStacks: ["Laravel", "PostgreSQL"],
+    },
+    {
+      image: "",
+      title: "EthiopicLingo Backend",
+      description:
+        "Shewa-Mart is an e-commerce platform that used CI/CD pipelines to optimize its development process, resulting in a 45% speed increase and the integration of Chapa for enhanced financial functions. The platform used Zustand and React for advanced state management and reusable components, with an emphasis on the user experience. Improvements to the architecture led to a significant 75% gain in system efficiency, and a flexible business dashboard helped to cut project development time by 50%.",
+      link: "https:/ethiopiclingo.com",
+      techStacks: [
+        "Fastify",
+        "MySQL",
+        "Swagger",
+        "NodeJS",
+        "Redis",
+        "BullMQ",
+        "Firebase",
+        "Nodemailer",
+      ],
+    },
+  ];
   return (
-    <div>
-      <h1>BackEnd</h1>
+    <div className="md:w-2/3 w-full flex flex-col gap-10">
+      {BackProjects.map((projects, index) => (
+        <ProjectCard
+          index={index}
+          key={index}
+          link={projects.link}
+          title={projects.title}
+          description={projects.description}
+          image={projects.image}
+          techStack={projects.techStacks}
+        />
+      ))}
     </div>
   );
 };
 export default BackEnd;
-export const BackProjects = [
-    {
-      image: "",
-      title: "Ethiochef Client Side & Admin Dashboard",
-      description:
-        "The food recipe web app integrated diverse tech and multiple payment methods for 200k+ users. Back-end optimization with Redis achieved a 60% performance boost, and CI/CD pipelines accelerated releases by 40%. The architecture supported customizable subscriptions, and React with Shadcn UI improved user engagement by 30%.The food recipe web app integrated diverse tech and multiple payment methods for 200k+ users. Back-end optimization with Redis achieved a 60% performance boost, and CI/CD pipelines accelerated releases by 40%. The architecture supported customizable subscriptions, and React with Shadcn UI improved user engagement by 30%.",
-      link: "https://ethiochef.com",
-      techStacks: [
-        "Front End",
-        "Back end",
-        "Nextjs",
-        "Tailwindcss",
-        "Shadcn UI",
-        "Express",
-        "Prisma",
-        "Mysql",
-      ],
-      githublink: "sample",
-    },
-    {
-      image: "",
-      title: "Digital Merkato Client Side & Admin Dashboard",
-      description:
-        "Digital Merkato, an E-commerce, streamlined development with CI/CD pipelines, boosting speed by 45%, and integrated payment methods like Chapa for enhanced financial capabilities. Prioritizing user experience, the platform utilized React and Zustand for reusable components and advanced state management. Architectural improvements led to a 75% increase in system efficiency, and a versatile company dashboard reduced project development time by 50%.",
-      link: "https://digitalmerkato.co",
-      techStacks: [
-        "Front End",
-        "Reactjs",
-        "Tailwindcss",
-        "Shadcn UI",
-        "Zustand",
-        "React Query",
-      ],
-      githublink: "sample",
-    },
-    {
-      image: "",
-      title: "Shewa-Mart Client Side & Admin Dashboard",
-      description:
-        "Shewa-Mart is an e-commerce platform that used CI/CD pipelines to optimize its development process, resulting in a 45% speed increase and the integration of Chapa for enhanced financial functions. The platform used Zustand and React for advanced state management and reusable components, with an emphasis on the user experience. Improvements to the architecture led to a significant 75% gain in system efficiency, and a flexible business dashboard helped to cut project development time by 50%.",
-      link: "https://shewa-mart.com",
-      techStacks: [
-        "Front End",
-        "Reactjs",
-        "Tailwindcss",
-        "Shadcn UI",
-        "Zustand",
-        "React Query",
-      ],
-    },
-    {
-      image: "",
-      title: "Edu Bus Admin Dashboard",
-      description:
-        "Edu Bus ensures safe and efficient school transportation, providing real-time tracking and financial tools for schools, parents, and car providers. Parents receive immediate feedback and secure payments, car providers benefit from streamlined navigation, and schools gain control through a dedicated dashboard. Overall, Edu Bus enhances the school transportation experience for all stakeholders.",
-      link: "https://education-bus-system-admin.vercel.app/",
-      techStacks: [
-        "Front End",
-        "Reactjs",
-        "Tailwindcss",
-        "Shadcn UI",
-        "Zustand",
-        "React Query",
-      ],
-      githublink: "smaple link",
-    },
-    {
-      image: "",
-      title: "Edu Bus Mobile App",
-      description:
-        "Edu Bus ensures safe and efficient school transportation, providing real-time tracking and financial tools for schools, parents, and car providers. Parents receive immediate feedback and secure payments, car providers benefit from streamlined navigation, and schools gain control through a dedicated dashboard. Overall, Edu Bus enhances the school transportation experience for all stakeholders.",
-      techStacks: [
-        "Front End",
-        "Reactjs",
-        "Tailwindcss",
-        "Shadcn UI",
-        "Zustand",
-        "React Query",
-      ],
-      githublink: "sample link",
-    },
-  ];
-  
